e61b4d60ce14204ec99c915899e49aa61d907d73
cleantted <seo.cleantted@gmail.com>
2023-10-14 01:15:11 +0900

    色々更新

--- library.py
+++ library.py
@@ -1,19 +1,38 @@
-import bisect
-from collections import deque
+
 import copy
 import heapq
-import sys
 import itertools
 import math
-import queue
-input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
-mod = 10 ** 9 + 7
+import operator
+import sys
+from bisect import bisect, bisect_left, bisect_right, insort
+from collections import Counter, deque
+from fractions import Fraction
+from functools import cmp_to_key, lru_cache, partial
+from inspect import currentframe
+from math import ceil, gcd, log10, pi, sqrt
 
-def read_values(): return map(int, input().split())
-def read_index(): return map(lambda x: int(x) - 1, input().split())
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
+input = sys.stdin.readline
+sys.setrecursionlimit(10000000)
+# mod = 10 ** 9 + 7
+mod = 998244353
+# mod = 1 << 128
+# mod = 10 ** 30 + 1
+INF = 1 << 61
+DIFF = 10 ** -9
+DX = [1, 0, -1, 0, 1, 1, -1, -1]
+DY = [0, 1, 0, -1, 1, -1, 1, -1]
+
+def read_values(): return tuple(map(int, input().split()))
+def read_index(): return tuple(map(lambda x: int(x) - 1, input().split()))
 def read_list(): return list(read_values())
-def read_lists(N): return [read_list() for n in range(N)]
+def read_lists(N): return [read_list() for _ in range(N)]
+def dprint(*values): print(*values, file=sys.stderr)
+def dprint2(*values):
+    names = {id(v): k for k, v in currentframe().f_back.f_locals.items()}
+    dprint(", ".join(f"{names.get(id(value), '???')}={repr(value)}" for value in values))
 
 
 class Val:
@@ -322,22 +341,18 @@ class SegmentTree:
     def query(self, l, r):
         l += self.M
         r += self.M
-        left_value = []
-        right_value = []
+        left_value = self.unit
+        right_value = self.unit
         while l < r:
             if l & 1:
-                left_value.append((self.T[l], l))
+                left_value = self.funct(left_value, self.T[l])
                 l += 1
             if r & 1:
-                right_value.append((self.T[r - 1], r - 1))
+                right_value = self.funct(self.T[r - 1], right_value)
             l >>= 1
             r >>= 1
         
-        left_value.extend(reversed(right_value))
-
-        res = self.unit
-        for lv, _ in left_value:
-            res = self.funct(res, lv)
+        res = self.funct(left_value, right_value)
 
         return res
 
